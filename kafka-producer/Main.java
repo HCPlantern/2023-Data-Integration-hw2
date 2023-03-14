f7d6599f502bf82c4ee0411ccb1e114953817aec
Yueming <201830115@smail.nju.edu.cn>
2023-03-14 10:34:02 +0800

    feat:格式化

--- kafka-producer/Main.java
+++ kafka-producer/Main.java
@@ -6,36 +6,36 @@
  * 行计数器兼做分区计数器，用于确保数据在分区的平均分配
  */
 public static void main(String[]args){
-        File dataPath = new File("~/data");
-        File[] tempList = dataPath.listFiles();
-        Properties kafkaProps = new Properties();
+        File dataPath=new File("~/data");
+        File[]tempList=dataPath.listFiles();
+        Properties kafkaProps=new Properties();
         kafkaProps.put("bootstrap.servers","localhost:9092");
         kafkaProps.put("key.serializer","org.apache.kafka.common.serialization.StringSerializer");
-        kafkaProps.put("value.serializer","org.apache.kafka.common.serialization.StringSerializer") ;
+        kafkaProps.put("value.serializer","org.apache.kafka.common.serialization.StringSerializer");
         kafkaProps.put("linger.ms","2000");
-        KafkaProducer<String, String> producer = new KafkaProducer<String,String>(kafkaProps);
+        KafkaProducer<String, String> producer=new KafkaProducer<String, String>(kafkaProps);
 
-        int sleep_counter = 0;
+        int sleep_counter=0;
         for(File f:tempList){
-        try {
-             FileInputStream fis = new FileInputStream(f);
-            BufferedReader br = new BufferedReader(new InputStreamReader(fis));
-            String readin = "";
-            while(true){
-                try {
-                    if ((readin = br.readLine())!=null){ProducerRecord<String,String> record = new ProducerRecord<>("mxdeal",sleep_counter&7,null,readin);
-                    producer.send(record);
-                    if(++sleep_counter==1024){
-                    sleep_counter = 0;
-                    Thread.sleep(50);
+        try{
+        FileInputStream fis=new FileInputStream(f);
+        BufferedReader br=new BufferedReader(new InputStreamReader(fis));
+        String readin="";
+        while(true){
+        try{
+        if((readin=br.readLine())!=null){ProducerRecord<String, String> record=new ProducerRecord<>("mxdeal",sleep_counter&7,null,readin);
+        producer.send(record);
+        if(++sleep_counter==1024){
+        sleep_counter=0;
+        Thread.sleep(50);
         }
         }else break;
-        } catch (Exception e) {
+        }catch(Exception e){
         e.printStackTrace();
         }
 
         }
-        } catch (FileNotFoundException e) {
+        }catch(FileNotFoundException e){
         e.printStackTrace();
         }
         }
