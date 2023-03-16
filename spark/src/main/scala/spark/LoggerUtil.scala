d03e397e6d85d7c4ee25b0bf7eab5a3ea3a188d2
Yueming <201830115@smail.nju.edu.cn>
2023-03-16 19:17:05 +0800

    feat:reformat code

--- spark/src/main/scala/spark/LoggerUtil.scala
+++ spark/src/main/scala/spark/LoggerUtil.scala
@@ -4,19 +4,19 @@ import org.apache.log4j.{Level, Logger}
 import org.apache.spark.internal.Logging
 
 object LoggerUtil extends Logging {
-  def setSparkLogLevels(level: Level = Level.WARN) {
-    val log4jInitialized = Logger.getRootLogger.getAllAppenders.hasMoreElements
-    if (!log4jInitialized) {
-      logInfo(s"Setting log level to [${level.toString}] for Spark")
-      Logger.getRootLogger.setLevel(level)
+    def setSparkLogLevels(level: Level = Level.WARN) {
+        val log4jInitialized = Logger.getRootLogger.getAllAppenders.hasMoreElements
+        if (!log4jInitialized) {
+            logInfo(s"Setting log level to [${level.toString}] for Spark")
+            Logger.getRootLogger.setLevel(level)
+        }
     }
-  }
 
-  def warn(str: String): Unit = {
-    logWarning(str)
-  }
+    def warn(str: String): Unit = {
+        logWarning(str)
+    }
 
-  def error(str: String): Unit = {
-    logError(str)
-  }
+    def error(str: String): Unit = {
+        logError(str)
+    }
 }
